f183c448ab5f69a7cea0b8f3ce8955e2b44883f8
TheAyushThakur7 <goodcontent1010@gmail.com>
2024-12-10 14:53:56 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 CORS(app)  # This enables CORS for all domains by default
 
 # Set AssemblyAI API key
-aai.settings.api_key = "ddb044374c7d4b9bb4b2f8f75de5047f"
+aai.settings.api_key = "ddb044374c7d4b9bb4b2f8f75de5047f"  # API key is still included as per your request
 
 @app.route('/transcribe', methods=['POST'])
 def transcribe():
@@ -26,4 +26,4 @@ def transcribe():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Default to 5000 if PORT isn't set
-    app.run(host='0.0.0.0', port=port)x
+    app.run(host='0.0.0.0', port=port)
